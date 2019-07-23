1d5f5234a6f63469a8799aa5dfd8ab13b5882979
Dmitri Smirnov <d.s@plexoos.com>
2019-07-23 15:55:53 -0400

    Minor updates

--- sync-star-cvs2git-cron.sh
+++ sync-star-cvs2git-cron.sh
@@ -3,17 +3,22 @@
 # This script can be run as a crontab job to sync STAR CVS repository with the
 # remote Git repository github.com:star-bnl/star-cvs.git
 # 
-# 0 0,8,12,20 * * * /path/to/my-sh-tk/sync-star-cvs2git-cron.sh &> /path/to/my-sh-tk/sync-star-cvs2git-cron.log
+# 0 2,8,20 * * * /path/to/my-sh-tk/sync-star-cvs2git-cron.sh &> /path/to/my-sh-tk/sync-star-cvs2git-cron.log
 #
 
 echo -- Start crontab job at
 date
 
+# Set the variables
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export CVSROOT="/afs/rhic.bnl.gov/star/packages/repository"
 export PREFIX="/scratch/smirnovd/star-bnl-readonly"
 export LOCAL_CVSROOT_DIR="${PREFIX}/star-cvs-local"
 
+
+#
+# Create or update a local copy of CVS repository
+#
 echo
 echo -- Step 1. Updating local copy of CVS repository in ${LOCAL_CVSROOT_DIR}/cvs
 ${SCRIPT_DIR}/sync-star-cvsgit.sh cvs rsync
@@ -24,6 +29,10 @@ echo -- Step 1a. Clean up local copy of CVS repository in ${LOCAL_CVSROOT_DIR}/c
 rm -fr ${LOCAL_CVSROOT_DIR}/cvs/StarDb/Geometry/tpc/tpcPadPlanes.dev2019.C,v
 echo -- Done
 
+
+#
+# Run cvs2git
+#
 echo
 echo -- Step 2. Creating Git blob files from the local CVS repository
 cvs2git --fallback-encoding=ascii --use-rcs --co=/usr/local/bin/co --force-keyword-mode=kept \
@@ -32,6 +41,10 @@ cvs2git --fallback-encoding=ascii --use-rcs --co=/usr/local/bin/co --force-keywo
         --username=cvs2git ${LOCAL_CVSROOT_DIR}/cvs &> ${PREFIX}/sync-star-cvs2git-cron-step2.log
 echo -- Done
 
+#
+# Re-create the actual git repository from the blob and dump files created build by cvs2git
+# Then checkout and push to the origin
+#
 export LOCAL_GIT_DIR="${PREFIX}/star-bnl/star-cvs"
 echo
 echo -- Step 3. Recreating Git repository in ${LOCAL_GIT_DIR}
